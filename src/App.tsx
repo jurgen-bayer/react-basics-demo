6684eb61461043507b34b401a75261acac4a5087
Jürgen Bayer <jb@juergen-bayer.net>
2022-03-28 21:22:29 +0100

    Add Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import './styles/styles.scss';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+// Import a Material UI icon
+import EmojiEmotions from '@mui/icons-material/EmojiEmotions';
+import { TextField, Typography } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 function App() {
-  return (
-    <h1>React Condensed Demo</h1>
-  );
+    const darkTheme = createTheme({
+        palette: {
+          mode: 'dark'
+        },
+      });
+    
+    return (
+        <ThemeProvider theme={darkTheme}>
+            {/* Enable Material UI typography (h1, h2 etc.) */}
+            <Typography>
+                <h1>React Condensed Demo</h1>
+                {/* Display an icon */}
+                <EmojiEmotions></EmojiEmotions>
+                <TextField id="standard-basic" label="Standard" variant="standard" />
+            </Typography>
+        </ThemeProvider>
+    );
 }
 
 export default App;
